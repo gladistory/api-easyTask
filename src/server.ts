15bc19e0bc8a69357061c390a287268783fa9808
Vinicius de Souza Santos <desouza850@gmail.com>
2025-01-11 18:50:12 -0300

    add

--- src/server.ts
+++ src/server.ts
@@ -11,5 +11,5 @@ app.use((req, res, next) => {
 const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}/api/tasks`);
 });
