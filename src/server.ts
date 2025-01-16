f18adcfa4f947286da7efa9dbe69b1f597223599
Vinicius de Souza Santos <desouza850@gmail.com>
2025-01-16 18:46:58 -0300

    up

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import dotenv from 'dotenv';
 import app from './app';
 
+// Carregar variáveis de ambiente
 dotenv.config();
 
+// Middleware para configurar headers de CORS
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Ajuste para a origem que desejar
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
+// Definir a porta, com fallback para 3306
+const PORT = Number(process.env.PORT) || 3306;
 
-/*
+// Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/api/tasks`);
 });
-*/
+
